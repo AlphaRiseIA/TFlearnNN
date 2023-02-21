9296d31bf2d8afcaed8770845e68bc39d6bd3d7a
AlphaRise <125960781+AlphaRiseIA@users.noreply.github.com>
2023-02-21 11:58:00 +0100

    Add files via upload

---
+++ TflearnNN.py
@@ -0,0 +1,22 @@
+#Ignore if all is squished
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+import tflearn as tfl
+import numpy as np
+import tensorflow as tf
+DATA_1 = np.array(x)
+DATA_2 = np.array(y)
+#------------------------------------------------------------
+#The NN here uwu
+netPear = tfl.input_data(shape=[None,])
+netPear = tfl.fully_connected(netPear,10)
+netPear = tfl.fully_connected(netPear,10)
+netPear = tfl.fully_connected(netPear,1,activation='relu')
+netPear = tfl.regression(netPear)
+coockie = tfl.DNN(netPear)
+#------------------------------------------------------------
+#Now we cook it and store it in a jar like cookies
+coockie.fit(DATA_1,DATA_2,n_epoch=1000,batch_size=11,show_metric=True)
+coockie.save("CoockiesJar.tflearn")
+#------------------------------------------------------------
